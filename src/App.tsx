77468affba9bb3d22677c79e37bff546445709ac
Esther Yu <estyuyui@gmail.com>
2025-04-18 17:58:20 -0400

    added components folder and instructions in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+// import your component here
 
 function App() {
 
   return (
     <>
+    {/* put your component here to see it when you run 'npm run dev' */}
     </>
   )
 }
