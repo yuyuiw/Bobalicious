5715fc2ec005f00d08889033beb1d2f387f75c98
ango06 <ngoangela6@gmail.com>
2025-04-20 17:01:25 -0400

    imported product overlay component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 // import your component here
+import ProductOverlay from './components/ProductOverlay';
 
 function App() {
 
   return (
     <>
     {/* put your component here to see it when you run 'npm run dev' */}
+    <ProductOverlay />
     </>
   )
 }
