97d47247359ad8a79d19dfd5df37d37d4410a4b0
SarayuJ3 <jilludumudisarayu@gmail.com>
2025-04-21 04:19:30 -0400

    Linked to Firebase, created client Product View. Seperate tsx file for functions that modify the boba: - i did get boba for future, - delete boba - get single boba flavor (would make seperate boba page easier)
    oh also added universal pink background into index.css but you can ignore that probably lol

---
+++ src/firebase/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyARNtfyssecTtDSUE1cSb76Kir4LBW2UVQ",
+  authDomain: "bobalicious-78b3b.firebaseapp.com",
+  projectId: "bobalicious-78b3b",
+  storageBucket: "bobalicious-78b3b.firebasestorage.app",
+  messagingSenderId: "82701198048",
+  appId: "1:82701198048:web:b53aa288e06c710d072685",
+  measurementId: "G-FM6W5B321S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
