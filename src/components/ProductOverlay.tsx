a2dec356e460bb4e301f03ae562ad592de67bc0d
ango06 <ngoangela6@gmail.com>
2025-04-21 22:38:05 -0400

    changed comments

--- src/components/ProductOverlay.tsx
+++ src/components/ProductOverlay.tsx
@@ -1,7 +1,6 @@
-// Angela's task
 import { Button, Modal } from '@mui/material';
 
-/** we would need to import this for the whole project? Or I can use something else */
+// imported icons from Material UI
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 import RemoveIcon from '@mui/icons-material/Remove';
@@ -9,7 +8,7 @@ import RemoveIcon from '@mui/icons-material/Remove';
 const ProductOverlay = () => {
     // placeholders 
     const image = "https://tyberrymuch.com/wp-content/uploads/2022/07/taro-milk-tea-recipe-1-735x735.jpg";
-    const price = 5.00; // how to decimal if we want it?
+    const price = 5.00;
     const description = "Purple and sweet";
     const ingredients = "Taro, milk, ice, boba";
     const quantity = 1;
@@ -19,27 +18,27 @@ const ProductOverlay = () => {
         <Modal open={open} className="flex justify-center items-center">
             <div className="bg-stone-100 w-120 p-8 text-black rounded-2xl">
                 <div className="flex relative justify-center">
-                    {/** Also allow close by clicking out of the modal? */}
+                    {/** add handleClick */}
                     <CloseIcon className="absolute left-0 m-0 cursor-pointer" />
                     <h2 className="text-center text-5xl mt-6 font-semibold">Taro Tea</h2>
                 </div>
 
-                {/** I can adjust the height to h-auto or whatever we want the dimensions to be later */}
                 <img src={image} className="my-8 mx-auto w-5/6 h-60" />
 
                 <div className="my-5 mx-4 text-left">
-                    <p className="my-1"><span className="text-red-700 font-bold italic">Price: </span> ${price}</p>
+                    <p className="my-1"><span className="text-red-700 font-bold italic">Price: </span> ${price.toFixed(2)}</p>
                     <p className="my-1"><span className="text-red-700 font-bold">Description: </span> {description}</p>
                     <p className="my-1"><span className="text-red-700 font-bold">Ingredients: </span> {ingredients}</p>
                 </div>
 
-                {/** Do we want an edit button for vendor view? */}
                 <div className="flex row justify-center items-center mt-8">
+                    {/** add handleClicks */}
                     <p className="m-5"><span className="text-red-700 font-bold">Qty: </span>{quantity}</p>
                     <Button sx={{ backgroundColor: '#F7DCE3', color: 'black', margin: '2px' }} variant="contained"><AddIcon /></Button>
                     <Button sx={{ backgroundColor: '#F7DCE3', color: 'black', margin: '2px' }} variant="contained"><RemoveIcon /></Button>
                 </div>
                 <div className="flex justify-center m-1">
+                    {/** add handleClick */}
                     <Button sx={{ backgroundColor: 'black', color: '#F7DCE3' }} variant="contained">Add to cart</Button>
                 </div>
             </div>  
