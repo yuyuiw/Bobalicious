9bd9d40a940bcbd5efdf39dc8945daeaa7732009
Esther Yu <estyuyui@gmail.com>
2025-04-23 22:07:10 -0400

    opening page done, 'get started' button navigates to login page

--- src/pages/OpeningPage.tsx
+++ src/pages/OpeningPage.tsx
@@ -1,13 +1,16 @@
+import { useNavigate } from 'react-router';
 import logo from '../images/logo.png';
 
 const OpeningPage = () => {
+  const navigate = useNavigate();
+
   return(
     <div className="flex flex-col justify-center items-center gap-5 w-screen h-screen">
       <h1 className="text-3xl font-[Lemonada]">
         Bobalicious
       </h1>
       <img src={logo} alt="bubble tea logo" className="size-50 border-solid border-4 rounded-full"/>
-      <button className="bg-[#2C2C2C] text-neutral-100 p-5 rounded-lg">Get Started</button>
+      <button onClick={() => navigate("/login")} className="bg-[#2C2C2C] text-neutral-100 p-5 rounded-lg">Get Started</button>
     </div>
   )
 }
