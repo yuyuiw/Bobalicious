7e6b60e720b11fbadb3eb558b1ba1b050d86cbf4
Esther Yu <estyuyui@gmail.com>
2025-04-24 00:30:25 -0400

    navbar is only in client mode rn, client catalog navbar and routing added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,12 +11,12 @@ const Navbar = () => {
         return <h1>Log-In</h1>;
       case '/signup':
         return <h1>Sign-Up</h1>;
-      case '/home':
+      case '/client-home':
         return <div></div>;
       default:
         return (
-          <div>
-            <a href="/">Home</a> | <a href="/edit-menu">Edit Menu</a> | <a href="/analytics">Analytics</a>
+          <div className="underline flex justify-evenly w-full">
+            <a href="/profile">Profile</a><a href="/cart">Cart</a><a href="/client-catalog">Menu</a><a href="/">Home</a>
           </div>
         );
     }
@@ -27,7 +27,8 @@ const Navbar = () => {
       {location.pathname !== "/" && (
         <div className="bg-[#cbe3ba] flex flex-col items-center py-12">
           <h1 className="text-4xl font-[Lemonada]">Bobalicious</h1>
-          <div className="text-2xl">
+          <br/>
+          <div className="flex justify-center text-2xl w-full">
           {getNavContent()}
           </div>
         </div>
