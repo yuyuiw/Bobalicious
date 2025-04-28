efa08d2ece5222e738ae27b9194cb345ae0abf08
Esther Yu <estyuyui@gmail.com>
2025-04-28 13:42:20 -0400

    updated home page

--- src/pages/VendorHomePage.tsx
+++ src/pages/VendorHomePage.tsx
@@ -5,18 +5,18 @@ const VendorHomePage = () => {
   return (
     <>
       <Navbar />
-      <div className="bg-pink-100 min-h-screen p-8">
+      <div className="p-8">
         <div className="text-center mb-8">
           <h2 className="italic text-2xl mb-4">Welcome, Vendor!</h2>
-          <p className="text-lg max-w-3xl mx-auto">
+          <p className="text-xl max-w-3xl mx-auto">
             Manage your menu, track orders, and serve up smiles. Let’s make every boba drop unforgettable!
           </p>
         </div>
 
-        <div className="flex justify-center gap-6">
+        <div className="flex justify-center gap-20">
           <Link
             to="/vendor-catalog"
-            className="bg-gray-800 text-white rounded-2xl p-6 w-60 h-85 flex flex-col items-center justify-center text-center shadow-md hover:bg-gray-700 transition"
+            className="bg-[#2C2C2C] text-white rounded-2xl p-6 w-70 h-85 flex flex-col items-center justify-center text-center shadow-md hover:opacity-90 transition"
           >
             <div className="font-bold text-lg mb-1">Menu</div>
             <div className="text-sm">Manage your boba creations</div>
@@ -24,10 +24,9 @@ const VendorHomePage = () => {
 
           <Link
             to="/"
-            className="bg-gray-800 text-white rounded-2xl p-6 w-60 h-85 flex flex-col items-center justify-center text-center shadow-md hover:bg-gray-700 transition"
+            className="bg-[#2C2C2C] text-white rounded-2xl p-6 w-70 h-85 flex flex-col items-center justify-center text-center shadow-md hover:opacity-90 transition"
           >
             <div className="font-bold text-lg mb-1">Sign Out</div>
-            <div className="text-sm">Logout of your account</div>
           </Link>
         </div>
       </div>
